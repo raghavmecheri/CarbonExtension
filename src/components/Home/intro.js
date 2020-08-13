7be8f58d4d453431d1026083b78d72587e67518a
Achiaga <alfonso.achiaga@gmail.com>
2020-08-13 14:59:32 +0200

    fix naming error

--- src/components/Home/intro.js
+++ src/components/Home/intro.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import IntroBackground from '../../assets/carbon-dioxide.svg';
+
 const Intro = ({ handleIntro }) => {
 	return (
 		<div>
 			<button onClick={handleIntro}>Start</button>
+			<img src={IntroBackground} alt='backgrnd' />
 		</div>
 	);
 };
